598dbf1669277290562bb50c1fafd5972c5770f7
Shane Scott <shaneradio@gmail.com>
2018-06-25 17:50:19 -0400

    working on axios request to QoD in Note.js

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 class Note extends Component {
+    getQuote () {
+        axios.get(http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1)
+        .then()
+    }
     render () {
         return (
             <div>
